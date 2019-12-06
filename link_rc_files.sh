ff4c36586ea3ec492bbf346cda257b1800a02047
Sean Sutherland <seanrsuther@gmail.com>
2019-12-05 19:55:14 -0800

    Add lines to .bashrc if missing

--- link_rc_files.sh
+++ link_rc_files.sh
@@ -1,6 +1,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo Running from $DIR
-ln -s $DIR/.bash_aliases $HOME/.bash_aliases
-ln -s $DIR/.vimrc $HOME/.vimrc
-ln -s $DIR/.inputrc $HOME/.inputrc
-#cat .bashrc >> $HOME/.bashrc
+ln -s $DIR/.bash_aliases ~/.bash_aliases
+ln -s $DIR/.vimrc ~/.vimrc
+ln -s $DIR/.inputrc ~/.inputrc
+
+if ! grep -F ". ~/.bash_aliases" .bashrc; then
+    echo "if [ -f ~/.bash_aliases ]; then" >> ~/.bashrc
+    echo "    . ~/.bash_aliases" >> ~/.bashrc
+    echo "fi" >> ~/.bashrc
+fi
+if ! grep -F ". ~/.psrc" .bashrc; then
+    echo "if [ -f ~/.psrc]; then" >> ~/.bashrc
+    echo "    . ~/.psrc" >> ~/.bashrc
+    echo "fi" >> ~/.bashrc
+fi
