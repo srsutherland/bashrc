277d82cde2df860972f828e51aca974840deac58
Sean Sutherland <SeanRSuther@gmail.com>
2020-11-01 12:17:19 -0800

    make the script tell you what it's doing

--- install/install-youtube-dl.sh
+++ install/install-youtube-dl.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo -e "\033[1;36m""> sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl" "\033[0m"
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+echo -e "\033[1;36m""> sudo chmod a+rx /usr/local/bin/youtube-dl" "\033[0m"
 sudo chmod a+rx /usr/local/bin/youtube-dl
+echo -e "\033[1;36m""> youtube-dl --version" "\033[0m"
+youtube-dl --version
