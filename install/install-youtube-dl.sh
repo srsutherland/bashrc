d13a2b0b52cf8c44df4b80b0bed1ce47ef21d234
Sean Sutherland <SeanRSuther@gmail.com>
2022-01-13 20:41:34 -0800

    change to youtube-dlp

--- install/install-youtube-dl.sh
+++ install/install-youtube-dl.sh
@@ -2,8 +2,8 @@
 
 echo -e "\033[1;36m""> youtube-dl --version" "\033[0m"
 youtube-dl --version
-echo -e "\033[1;36m""> sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl" "\033[0m"
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+echo -e "\033[1;36m""> sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/youtube-dl" "\033[0m"
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/youtube-dl
 echo -e "\033[1;36m""> sudo chmod a+rx /usr/local/bin/youtube-dl" "\033[0m"
 sudo chmod a+rx /usr/local/bin/youtube-dl
 echo -e "\033[1;36m""> youtube-dl --version" "\033[0m"
