c7201a51de18e40f637a3f54f2572856da9eeca9
Sean Sutherland <seanrsuther@gmail.com>
2022-12-05 15:27:53 -0800

    Update profile.ps1

--- win/profile.ps1
+++ win/profile.ps1
@@ -1,10 +1,6 @@
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-function cdp      { cd P:\ }
-function cdpsndev { cd P:\SnDev\ }
-function cdsndev  { cd C:\Sn\SnDev\ }
-function cdcommon { cd P:\Common\SRS\ }
-function cdsrs    { cd P:\Common\SRS\ }
+function cdd  { cd $HOME\Downloads }
 
 #https://gist.github.com/timsneath/19867b12eee7fd5af2ba
 # Useful shortcuts for traversing directories
@@ -38,18 +34,27 @@ function admin
 # with elevated rights. 
 Set-Alias -Name su -Value admin
 Set-Alias -Name sudo -Value admin
-
 #https://stackoverflow.com/questions/138144/what-s-in-your-powershell-profile-ps1-file
 function Get-Time { return $(get-date | foreach { $_.ToLongTimeString() } ) }
-function prompt
-{
-    # Write the time 
-    write-host "[" -noNewLine
-    write-host $(Get-Time) -foreground yellow -noNewLine
-    write-host "] " -noNewLine
-    # Write the path
-    write-host $($(Get-Location).Path.replace($home,"~").replace("\","/")) -foreground green -noNewLine
-    write-host $(if ($nestedpromptlevel -ge 1) { '>>' }) -noNewLine
-    return "> "
+
+if (Get-Module -ListAvailable -Name posh-git) {
+    Import-Module posh-git
+    $GitPromptSettings.DefaultPromptPrefix.Text = '[$(Get-Date -f "MM-dd HH:mm:ss")] '
+    $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::gray
+    $GitPromptSettings.DefaultPromptPath.ForegroundColor = [ConsoleColor]::green
+    $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 }
-Import-Module posh-git
+else {
+    #https://stackoverflow.com/questions/138144/what-s-in-your-powershell-profile-ps1-file
+    function prompt
+    {
+        # Write the time 
+        write-host "[" -noNewLine
+        write-host $(Get-Time) -foreground yellow -noNewLine
+        write-host "] " -noNewLine
+        # Write the path
+        write-host $($(Get-Location).Path.replace($home,"~").replace("\","/")) -foreground green -noNewLine
+        write-host $(if ($nestedpromptlevel -ge 1) { '>>' }) -noNewLine
+        return "> "
+    }
+}
